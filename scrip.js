dbfa96b59d5fc734e51e0dd5377e9d21b0e4587a
williamvasconcelos2023 <patraoeolema@gmail.com>
2023-10-03 17:30:32 -0300

    input

--- scrip.js
+++ scrip.js
@@ -4,9 +4,17 @@ function generateNumber(){
 const min = Math.ceil(document.querySelector(".meu-imput").value)
 const max = Math.floor(document.querySelector(".meu-imput-max").value)
 
-const result =Math.floor(Math.random() * (max - min + 1)) + min;
+console. log(min, max)
+
+if(min >= max){
+    alert("O valor mínimo tem que ser MENOR que o valor máximo!")
+} else{
+    const result =Math.floor(Math.random() * (max - min + 1)) + min;
 
 alert(result)
+
+}
+
 
 }
 
